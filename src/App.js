e81762cc40a4aa67d2b0085707ef0411f06233dd
conconartist <conconart.info@gmail.com>
2021-04-11 18:23:51 -0600

    Build App component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-  const openSection = () => {
-    console.log('open this section')
+  const [openDevSection, setOpenDevSection] = useState(false);
+  const [openMusicSection, setOpenMusicSection] = useState(false);
+  const [openVocalSection, setOpenVocalSection] = useState(false);
+  const [openSewSection, setOpenSewSection] = useState(false);
+
+  const toggleDevSection = () => {
+    if(openDevSection === false) {
+      setOpenDevSection(true)
+    } else {
+      setOpenDevSection(false)
+    }
   }
+
+  const toggleMusicSection = () => {
+    if(openMusicSection === false) {
+      setOpenMusicSection(true)
+    } else {
+      setOpenMusicSection(false)
+    }
+  }
+
+  const toggleVocalSection = () => {
+    if(openVocalSection === false) {
+      setOpenVocalSection(true)
+    } else {
+      setOpenVocalSection(false)
+    }
+  }
+
+  const toggleSewSection = () => {
+    if(openSewSection === false) {
+      setOpenSewSection(true)
+    } else {
+      setOpenSewSection(false)
+    }
+  }
+
   return (
     <main>
       <h1>Conconart</h1>
       <div className='linkContainer'>
-        <div className='mainLink devLink' onClick={openSection}>
-          Software Engineer
-          <section className='linkSection'>
-            <p>Something goes here</p>
-          </section>
+        <div className='mainLink devLink' onClick={toggleDevSection}>
+          <h2>Software Engineer</h2>
+          {openDevSection &&
+            <section className='linkSection'>
+              <p>Something goes here</p>
+              <button className='buttonOne'>Portfolio</button>
+            </section>
+          }
         </div>
-        <div className='mainLink musicianLink' onClick={openSection}>
-          Musician
-          <section className='linkSection'>
-            <p>Something goes here</p>
-          </section>
+        <div className='mainLink musicianLink' onClick={toggleMusicSection}>
+          <h2>Musician</h2>
+          {openMusicSection && 
+            <section className='linkSection'>
+              <p>Something goes here</p>
+              <button className='buttonOne'>Fair Elle</button>
+              <button className='buttonTwo'>Ivory Circle</button>
+            </section>
+          }
         </div>
-        <div className='mainLink vocalLink' onClick={openSection}>
-          Vocal Coach
-          <section className='linkSection'>
-            <p>Something goes here</p>
-          </section>
+        <div className='mainLink vocalLink' onClick={toggleVocalSection}>
+          <h2>Vocal Coach</h2>
+          {openVocalSection && 
+            <section className='linkSection'>
+              <p>Something goes here</p>
+              <button className='buttonOne'>Voice Lessons</button>
+              <button className='buttonTwo'>Studio Coaching</button>
+            </section>
+          }
         </div>
-        <div className='mainLink sewistLink' onClick={openSection}>
-          Sewist
-          <section className='linkSection'>
-            <p>Something goes here</p>
-          </section>
+        <div className='mainLink sewistLink' onClick={toggleSewSection}>
+          <h2>Sewist</h2>
+          {openSewSection && 
+            <section className='linkSection'>
+              <p>Something goes here</p>
+              <button className='buttonOne'>Refashions</button>
+              <button className='buttonTwo'>MeMades</button>
+            </section>
+          }
         </div>
       </div>
       <section>
