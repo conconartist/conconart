34a7bd79d9eebbc4e3579b6e2683b54cf610276e
conconartist <conconart.info@gmail.com>
2021-05-25 15:02:22 -0600

    Add project gif

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import bookwormintro from '../assets/bookwormintro.gif';
 
 export const Portfolio = () => {
-    //link to Home
     //Bookworm and description
-    //screenshot/ gif
     //link to github
     //link to deployed
     //tech stack
@@ -33,11 +32,32 @@ export const Portfolio = () => {
         <>
           <h2 className='linkedSectionTitle'>Portfolio</h2>
           <section className='linkedSection'>
-            <h3>BookWorm</h3>
-            <h3>Lyric Lava</h3>
-            <h3>FaceIt</h3>
-            <h3>Swanson Sayings</h3>
-            <h3>Rancid Tomatillos</h3>
+            <div className='projectSection'>
+              <h3>BookWorm</h3>
+              <p className='projectSummary'>A web application that tracks reading progress, aimed at young readers.</p>
+              <img src={bookwormintro}></img>
+              <h4 className='techStackTitle'>Tech Stack:</h4>
+              <ul className='projectTechStack'>
+                <li>React</li>
+                <li>SCSS</li>
+              </ul>
+            </div>
+            <div className='projectSection'>
+              <h3>Lyric Lava</h3>
+              <h4 className='techStackTitle'>Tech Stack:</h4>
+            </div>
+            <div className='projectSection'>
+              <h3>FaceIt</h3>
+              <h4 className='techStackTitle'>Tech Stack:</h4>
+            </div>
+            <div className='projectSection'>
+              <h3>Swanson Sayings</h3>
+              <h4 className='techStackTitle'>Tech Stack:</h4>
+            </div>
+            <div className='projectSection'>
+              <h3>Rancid Tomatillos</h3>
+              <h4 className='techStackTitle'>Tech Stack:</h4>
+            </div>
           </section>
 
           <Link to='/'>
