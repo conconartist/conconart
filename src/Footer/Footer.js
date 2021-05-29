3e19e0ff3cdbb8a6e43e92bb288d4fae22c76c62
conconartist <conconart.info@gmail.com>
2021-05-28 19:26:02 -0600

    Update SCSS variables

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,7 +7,7 @@ import Email from '../Email/Email';
 export const Footer = () => {
   return (
     <section className='summaryContainer'>
-      <p>Connie Hong is a software developer, vocalist, songwriter, teacher, lifelong learner, and creative thinker.  She is the lead singer and songwriter for Ivory Circle and Fair Elle, and vocal coach in Denver, Colorado.</p>
+      <p><b>Connie Hong</b> is a software developer, vocalist, songwriter, teacher, lifelong learner, and creative thinker.  She is the lead singer and songwriter for Ivory Circle and Fair Elle, and vocal coach in Denver, Colorado.</p>
       <div className='contactContainer'>
         <div className='socialMediaContainer'>
           <a href='https://github.com/conconartist'><Github /></a>
