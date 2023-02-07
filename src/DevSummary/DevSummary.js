b5fcc19a44ec61d23e3ba1afdb8537d971b599f4
conconartist <conconart.info@gmail.com>
2023-02-06 21:46:38 -0700

    Update dev summary

--- src/DevSummary/DevSummary.js
+++ src/DevSummary/DevSummary.js
@@ -4,7 +4,7 @@ export const DevSummary = () => {
 
     return (
       <section className='devContainer'>
-        <p>As a software engineer, Connie is a graduate from <a href='https://turing.edu/'>Turing School of Software and Design</a> with a certification in Front-End Engineering.
+        <p>As a software engineer, Connie has been a full-time developer for Radial Development Group since September 2021.  She is a graduate from <a href='https://turing.edu/'>Turing School of Software and Design</a> with a certification in Front-End Engineering.
         </p>
         <h3>Technologies:</h3>
         <ul className='techStackList'>
@@ -12,10 +12,12 @@ export const DevSummary = () => {
           <li>HTML</li>
           <li>CSS/ SCSS (Sass)</li>
           <li>React</li>
-          <li>TDD (Mocha, Chai, Jest, Cypress)</li>
+          <li>Vue</li>
+          <li>TDD (Mocha, Chai, Jest, Cypress, Rspec)</li>
           <li>UI/UX (User Experience/ User Interface Design)</li>
           <li>Wireframing</li>
           <li>Project Planning (GitHub Projects, Kanban)</li>
+          <li>Project Management</li>
           <li>Figma</li>
           <li>Twitter</li>
           <li>Remote Learning/ Working</li>
@@ -25,7 +27,7 @@ export const DevSummary = () => {
           <li>GSAP (Greensock) Animation</li>
           <li>WordPress</li>
           <li>Express</li>
-          <li>MySQL</li>
+          <li>SQL</li>
           <li>Node.js</li>
           <li>Ruby on Rails</li>
           <li>PHP</li>
